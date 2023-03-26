f1ffe076ad0c4e586e381bc6f444ad5497d47f48
Ubuntu <ubuntu@ip-172-31-11-193.eu-north-1.compute.internal>
2023-03-26 07:45:55 +0000

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,14 +3,16 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://1682eb3b1476407887a849c93b81c814.vfs.cloud9.eu-north-1.amazonaws.com/rails/mailers/user_mailer/account_activation
   def account_activation
-    user = User.first
+    user = User.last
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # Preview this email at http://1682eb3b1476407887a849c93b81c814.vfs.cloud9.eu-north-1.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.last
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
